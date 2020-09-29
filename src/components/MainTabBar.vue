d1554b7c1a5d772a1295822a4e4e3edd16980b05
wdc <1812991192@qq.com>
2020-09-29 16:29:43 +0800

    icon2

--- src/components/MainTabBar.vue
+++ src/components/MainTabBar.vue
@@ -5,47 +5,39 @@
   <svg  slot="item-icon" class="icon" aria-hidden="true">
     <use xlink:href="#icon-shouye"></use>
   </svg>    
-  <!-- <span slot="item-icon" class="iconfont  icon-shouye2 "></span> -->
   <svg  slot="item-iconActive" class="icon" aria-hidden="true">
     <use xlink:href="#icon-shouye1"></use>
   </svg>
-  <!-- <span  slot="item-iconActive" class="iconfont  icon-shouye1 "></span> -->
     <div slot="item-text">首页</div>
   </TabItem>
   <!-- 2 -->
   <TabItem path="/about"  activeColor="#ff0">
      <svg  slot="item-icon" class="icon" aria-hidden="true">
       <use xlink:href="#icon-guanyu1"></use>
-    </svg>
-      <!-- <span slot="item-icon" class=" iconfont icon-dianying"></span> -->
+    </svg>    
     <svg  slot="item-iconActive" class="icon" aria-hidden="true">
       <use xlink:href="#icon-guanyu"></use>
-    </svg>
-      <!-- <span slot="item-iconActive" class=" iconfont icon-guanyu"></span> -->
+    </svg>   
       <div slot="item-text"> 关于</div>
   </TabItem>
   <!-- 3 -->
   <TabItem path="/movie"  activeColor="#ff0">
       <svg  slot="item-icon" class="icon" aria-hidden="true">
          <use xlink:href="#icon-dianying"></use>
-      </svg>
-      <!-- <span slot="item-icon" class=" iconfont icon-dianying"></span> -->
+      </svg>      
       <svg  slot="item-iconActive" class="icon" aria-hidden="true">
         <use xlink:href="#icon-dianying1"></use>
-      </svg>
-      <!-- <span slot="item-iconActive" class=" iconfont icon-dianying1"></span> -->
+      </svg>     
       <div slot="item-text">电影</div>
   </TabItem>
   <!-- 4 -->
   <TabItem path="/profile"  activeColor="#ff0">
       <svg  slot="item-icon" class="icon" aria-hidden="true">
         <use xlink:href="#icon-dianying"></use>
-      </svg>
-      <!-- <span slot="item-icon" class=" iconfont icon-wode"></span> -->
+      </svg>    
       <svg  slot="item-iconActive" class="icon" aria-hidden="true">
         <use xlink:href="#icon-dianying1"></use>
-      </svg>
-      <!-- <span  slot="item-iconActive" class=" iconfont icon-wode1"></span> -->
+      </svg>     
       <div slot="item-text">我的</div>
   </TabItem>
 </Tabbar> 
